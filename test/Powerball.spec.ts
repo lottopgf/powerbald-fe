89644c7fc68dc8d6a0020ebc02198c85fe455062
Kevin Charm <kevin@kevincharm.com>
2023-08-13 00:16:48 +0200

    implement jackpot

--- test/Powerball.spec.ts
+++ test/Powerball.spec.ts
@@ -7,7 +7,7 @@ import {
     BytecodeDeployer__factory,
     MockChainlinkVRFV2Randomiser,
 } from '../typechain-types'
-import { AbiCoder, Contract, Interface, solidityPacked } from 'ethers'
+import { AbiCoder, Contract, Interface, parseEther, solidityPacked } from 'ethers'
 import { expect } from 'chai'
 import { MockChainlinkVRFV2Randomiser__factory } from '../typechain-types/factories/contracts/test'
 
@@ -22,8 +22,10 @@ describe('Powerball', () => {
     let bytecodeDeployer: BytecodeDeployer
     let powerball: Contract
     let mockRandomiser: MockChainlinkVRFV2Randomiser
+    let feeRecipient: string
     beforeEach(async () => {
         ;[deployer] = await ethers.getSigners()
+        feeRecipient = ethers.getAddress(ethers.hexlify(ethers.randomBytes(20)))
 
         // mocks
         mockRandomiser = await new MockChainlinkVRFV2Randomiser__factory(deployer).deploy()
@@ -32,11 +34,13 @@ describe('Powerball', () => {
         bytecodeDeployer = await new BytecodeDeployer__factory(deployer).deploy()
         // Deploy Fe contracts
         const args = AbiCoder.defaultAbiCoder().encode(
-            ['address', 'uint8', 'uint256'],
+            ['address', 'uint8', 'uint256', 'uint256', 'address'],
             [
                 await mockRandomiser.getAddress() /** randomiser */,
                 69 /** ball_domain */,
                 3600 /** game duration =1h */,
+                parseEther('1') /** entry price */,
+                feeRecipient,
             ]
         )
         const tx = await bytecodeDeployer
@@ -62,8 +66,11 @@ describe('Powerball', () => {
             ['uint8', 'uint8', 'uint8', 'uint8', 'uint8'],
             [0, 20, 12, 68, 54]
         )
-        await powerball.enter(picks)
+        await powerball.enter(picks, {
+            value: parseEther('1'),
+        })
         expect(await powerball.entries_count(1)).to.eq(1)
+        expect(await ethers.provider.getBalance(feeRecipient)).to.eq(parseEther('0.5'))
     })
 
     it('rejects invalid picks', async () => {
@@ -71,7 +78,11 @@ describe('Powerball', () => {
             ['uint8', 'uint8', 'uint8', 'uint8', 'uint8'],
             [0, 20, 12, 69 /** too high */, 54]
         )
-        await expect(powerball.enter(picks)).to.be.reverted
+        await expect(
+            powerball.enter(picks, {
+                value: parseEther('1'),
+            })
+        ).to.be.reverted
     })
 
     it('interacts with vrf', async () => {
@@ -80,7 +91,9 @@ describe('Powerball', () => {
             ['uint8', 'uint8', 'uint8', 'uint8', 'uint8'],
             [0, 20, 12, 68, 54]
         )
-        await powerball.enter(picks)
+        await powerball.enter(picks, {
+            value: parseEther('1'),
+        })
 
         const requestId = await mockRandomiser.nextRequestId()
         const gameNum = await powerball.games_count()
@@ -96,7 +109,9 @@ describe('Powerball', () => {
             ['uint8', 'uint8', 'uint8', 'uint8', 'uint8'],
             [0, 20, 12, 68, 54]
         )
-        await powerball.enter(picks)
+        await powerball.enter(picks, {
+            value: parseEther('1'),
+        })
         // Fail to draw
         await expect(powerball.draw()).to.be.reverted
     })
